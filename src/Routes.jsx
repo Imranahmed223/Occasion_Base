2c1dc3f56115d5a7e253f42f76258303d08713ab
Abdul Wahab <102620981+wahab315@users.noreply.github.com>
2023-03-08 00:27:00 +0500

    new code

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import LayOut from "./commonComponents/LayOut/LayOut";
-// import CreateEvent from "./module/Vender/CreateEvent/CreateEvent";
-// import DashBoard from "./module/Vender/DashBoard/DashBoard";
-// import Settings from "./module/Vender/Settings/Settings";
-import { Setting, Service } from "./module";
+import { Layout } from "./commonComponents";
+import { Setting, Service, DashBoard } from "./module/Vender";
 
 const Routess = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="" element={<LayOut />}>
+          <Route path="" element={<Layout />}>
             <Route path="/dashboard" element={<DashBoard />}></Route>
-            {/* <Route path="/create_event" element={<CreateEvent />}></Route> */}
             <Route path="/settings" element={<Setting />}></Route>
             <Route path="/service" element={<Service />}></Route>
           </Route>
--- src/module/Vender/index.js
+++ src/module/Vender/index.js
@@ -1,2 +1,3 @@
 export { default as Setting } from "./Settings/Settings";
 export { default as Service } from "./Services/Services";
+export { default as DashBoard } from "./DashBoard/DashBoard";
